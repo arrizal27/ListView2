f9b86fabeeb76c1e9d142fe124754118dd673a1a
Fajar Sidiq Arrizal <fajarsidiq3000@gmail.com>
2020-09-13 18:26:07 +0700

    First Commit

---
+++ app/src/main/java/com/jar/listview/MainActivity.java
@@ -0,0 +1,40 @@
+package com.jar.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+//a List of type hero for holding list items
+    java.util.List<Hero> heroList;
+    //the listview
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //initializing objects
+        heroList = new ArrayList<>();
+        listView = (ListView) findViewById(R.id.listView);
+
+        //adding some values to our list
+        heroList.add(new Hero(R.drawable.spiderman, "Spiderman", "Avengers"));
+        heroList.add(new Hero(R.drawable.joker, "Joker", "InjusticeGang"));
+        heroList.add(new Hero(R.drawable.ironman, "Iron Man", "Avengers"));
+        heroList.add(new Hero(R.drawable.doctorstrange, "Doctor Strange", "Avengers"));
+        heroList.add(new Hero(R.drawable.captainamerica, "Captain America", "Avengers"));
+        heroList.add(new Hero(R.drawable.batman, "Batman", "Justice League"));
+
+        //creating the adapter
+        ListAdapter adapter = new ListAdapter(this, R.layout.custom_list, heroList);
+
+        //attaching adapter to the listview
+        listView.setAdapter(adapter);
+    }
+}
